3fba81d77652119daab76bf14578b52013030bea
lLipe-Brl <125685453+lLipe-Brl@users.noreply.github.com>
2023-04-14 13:51:58 -0300

    Add files via upload

---
+++ Exemplo03.java
@@ -0,0 +1,16 @@
+//Cáculo da área do Triângulo
+
+public class Exemplo03 {
+    
+public static final double BASE = 5.5; // A base é uma constante 
+
+public static void main(String[] args) {
+
+    double altura = 4.0;
+    double area = (BASE*altura)/2;
+
+    System.out.println("A área do triângulo é: " +area+ "cm²");
+
+}
+
+}
